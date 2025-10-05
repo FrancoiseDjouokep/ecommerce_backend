9e1071670ca9f11344bc5a75fa6fa0962367e1c0
Leslie Francoise <francoiseleslie05@gmail.com>
2025-10-05 06:42:31 +0100

    v3 du backend with internal render URL + ssl

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,8 @@ async function bootstrap() {
     console.log('Superadmin déjà existant.');
   }
 
-  const port = process.env.PORT || process.env.APP_PORT || 3000;
+  const port = process.env.PORT || 3000;
+
   await app.listen(port);
   console.log(` Application is running on port ${port}`);
 
